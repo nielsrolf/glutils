c1f89408ce2435d09630c64cc577e8bea1858001
Niels Warncke <niels.warncke@gmail.com>
2025-09-24 00:10:44 +0200

    cmt

--- glutils/__init__.py
+++ glutils/__init__.py
@@ -1,8 +1,27 @@
+# Copy of 'global utils' - a bunch will be irrelevant for this project
+# https://github.com/nielsrolf/glutils/blob/main/glutils/__init__.py
 import json
 import os
 from collections import UserDict
 
 import pandas as pd
+from glob import glob
+
+import asyncio
+from typing import Dict, Any, Coroutine
+
+async def gather_dict(coros: Dict[Any, Coroutine]) -> Dict[Any, Any]:
+    keys = list(coros.keys())
+    results = await asyncio.gather(*coros.values())
+    return dict(zip(keys, results))
+
+
+def apply_to_files(f, glob_pattern, **kwargs):
+    """Apply function f to all files matching glob_pattern
+    Assumes f takes a `path` argument and any additional kwargs."""
+    for path in glob(glob_pattern, recursive=True):
+        f(path, *kwargs)
+
 
 def load_jsonl(jsonl_path):
     with open(jsonl_path, 'r') as f:
@@ -10,7 +29,9 @@ def load_jsonl(jsonl_path):
 
 
 def write_jsonl(data, path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
+    try:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    except: pass
     with open(path, 'w') as f:
         for item in data:
             f.write(json.dumps(item) + '\n')
@@ -32,7 +53,6 @@ def load_csv(csv_path):
 
 
 def load(path):
-    print(f"Loading {path}")
     if path.endswith('.json'):
         return load_json(path)
     elif path.endswith('.jsonl'):
@@ -44,11 +64,17 @@ def load(path):
         data = load(os.path.join(path, files[0]))
         if isinstance(data, list):
             for file in files[1:]:
-                data += load(os.path.join(path, file))
+                try:
+                    data += load(os.path.join(path, file))
+                except:
+                    print(f"Failed to load {file}")
             return data
         elif isinstance(data, dict):
             for file in files[1:]:
-                data.update(load(os.path.join(path, file)))
+                try:
+                    data.update(load(os.path.join(path, file)))
+                except:
+                    print(f"Failed to load {file}")
             return data
     else:
         raise ValueError(f"Unknown file type: {path}")
@@ -59,6 +85,8 @@ def write(data, path):
         return write_json(data, path)
     elif path.endswith('.jsonl'):
         return write_jsonl(data, path)
+    elif path.endswith('.csv'):
+        return pd.DataFrame(data).to_csv(path, index=False)
     else:
         raise ValueError(f"Unknown file type: {path}")
     
@@ -117,3 +145,72 @@ def dict_recursive(fn):
     wrapped_fn.__name__ = fn.__name__
     return wrapped_fn
   
+
+def extract(text, tag):
+    text = text.split(f'<{tag}>')[1].split(f'</{tag}>')[0].strip()
+    try:
+        return float(text)
+    except:
+        return text
+
+def extract_tags(text, tags, prefix=''):
+    values = {}
+    for tag in tags:
+        values[prefix + tag] = extract(text, tag)
+    return values
+
+
+import diskcache as dc
+import hashlib
+
+
+def recursive_access(d, keys):
+    if d is None:
+        return None
+    if len(keys) == 1:
+        return d[keys[0]]
+    return recursive_access(d[keys[0]], keys[1:])
+
+
+cache = None
+# decorator for caching
+def cache_on_disk(*cache_by_args):
+    global cache
+    if cache is None:
+        cache = dc.Cache("cache")
+    def decorator(func):
+        import asyncio
+        
+        def get_cache_key(*args, **kwargs):
+            if not cache_by_args:
+                # If no specific args are provided, use all args and kwargs
+                key_parts = list(args) + [f"{k}={v}" for k, v in kwargs.items()]
+            else:
+                key_parts = [recursive_access(kwargs, arg.split('.')) for arg in cache_by_args]
+            
+            key = "-".join([str(i) for i in key_parts])
+            return hashlib.md5(key.encode()).hexdigest()
+
+        if asyncio.iscoroutinefunction(func):
+            async def wrapped(*args, **kwargs):
+                key = get_cache_key(*args, **kwargs)
+                
+                if key in cache:
+                    return cache[key]
+                result = await func(*args, **kwargs)
+                cache[key] = result
+                return result
+        else:
+            def wrapped(*args, **kwargs):
+                key = get_cache_key(*args, **kwargs)
+                
+                if key in cache:
+                    return cache[key]
+                result = func(*args, **kwargs)
+                cache[key] = result
+                return result
+        
+        wrapped.__name__ = func.__name__
+        return wrapped
+    
+    return decorator
